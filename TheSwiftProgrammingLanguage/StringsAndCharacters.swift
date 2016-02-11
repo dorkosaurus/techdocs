55f8ccbc2f5abc84d7c16387d33268823aa1628a
personal <vramaswamy@gmail.com>
2016-02-11 13:07:24 -0800

    finished chapter

--- TheSwiftProgrammingLanguage/StringsAndCharacters.swift
+++ TheSwiftProgrammingLanguage/StringsAndCharacters.swift
@@ -28,4 +28,60 @@ var char:Character = "h"
  /* char="hello" */
 */
 
+//append a character to a string
+var welcome = "Welcome Vivek"
+var ep:Character = "!"
+welcome.append(ep)
+//this doesn't work since ep and welcome are of different types
+//var welcome2 = welcome + ep
+
+/* SWIFT STRINGS BUILT FROM UNICODE SCALARS (21-bit number for character or modifier)
+*/
+
+//extended graphene clusters allow for _any_ character representation
+var theUSFlagCharacter:Character = "\u{1F1Fa}\u{1F1F8}"
+print(theUSFlagCharacter)
+
+/** ACCESS AND MODIFY STRING **/
+
+var stringWithIndex="the quick brown fox jumps over the lazy dog"
+print(stringWithIndex.startIndex)
+print(stringWithIndex.endIndex)
+print(stringWithIndex.endIndex.predecessor().predecessor())
+var char2 = stringWithIndex[stringWithIndex.startIndex]
+print(char2)
+
+//string indices
+var indices = stringWithIndex.characters.indices
+for index in indices{
+	print("for index= \(index) character=\(stringWithIndex[index])")
+}
+
+//insert character at index
+stringWithIndex.insert("!",atIndex:stringWithIndex.endIndex)
+print(stringWithIndex)
+
+//insert string at index
+stringWithIndex.insertContentsOf(" there".characters,at:welcome.endIndex)
+print(stringWithIndex)
+
+//remove at index
+stringWithIndex.removeAtIndex(welcome.startIndex)
+print(stringWithIndex)
+
+//remove a set of characters
+let range = stringWithIndex.endIndex.advancedBy(-6)..<stringWithIndex.endIndex
+stringWithIndex.removeRange(range)
+print(stringWithIndex)
+
+/** 
+NOTE THAT hasPrefix and hasSuffix relies upon NSString object behind the 
+scenes...does not work on ubuntu 
+	/*
+	var stringWithPrefixAndSuffix="Help I need somebody"
+	if stringWithPrefixAndSuffix.hasPrefix("Help"){
+		print("has prefix")
+	}
+	*/
+*/
 
